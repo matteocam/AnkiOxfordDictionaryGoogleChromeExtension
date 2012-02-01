ae8cd382f3aaff219740d0a6386d2ebf201b97f8
Ilia Andreev <varvaluk@gmail.com>
2012-02-02 01:30:04 +0400

    Updated to 0.2.4 version
    Allow to set with correction visibility
    Allow to set the Anki's functionality state
    Checks and alerts were added

--- js/startup.js
+++ js/startup.js
@@ -10,6 +10,7 @@ chrome.extension.onRequest.addListener(function (request, sender, sendResponse) 
         if (newDiv == null)
             newDiv = document.createElement('div');
         newDiv.setAttribute('id', divId);
+        newDiv.setAttribute('style', 'z-index:1000');
 
         var iframe = document.getElementById(frameId);
         if (iframe != null)
@@ -17,14 +18,14 @@ chrome.extension.onRequest.addListener(function (request, sender, sendResponse) 
 
         iframe = document.createElement("iframe");
         iframe.src = chrome.extension.getURL("process.html") + "?word=" + encodeURI(request.word);
-        iframe.setAttribute('style', 'width: 500px; height: 310px; border: black 1px solid');
+        iframe.setAttribute('style', 'width: 500px; height: 310px; border: 0; z-index: 1000;');
         iframe.setAttribute('id', frameId);
-        newDiv.appendChild(iframe)
+        newDiv.appendChild(iframe);
 
         document.body.appendChild(newDiv);
 
         var script = document.createElement("script");
-        script.innerHTML = "var googlechromeextensioncontentdiv = document.getElementById('" + divId + "');  googlechromeextensioncontentdiv.setAttribute('style', 'position:absolute; left:' + (googlechromeextensionpx - 280) + 'px; top: ' + (googlechromeextensionpy - 50) + 'px;');";
+        script.innerHTML = "var googlechromeextensioncontentdiv = document.getElementById('" + divId + "');  googlechromeextensioncontentdiv.setAttribute('style', 'position:absolute; left:' + (googlechromeextensionpx - 100) + 'px; top: ' + (googlechromeextensionpy - 50) + 'px; z-index:1000;');";
         document.body.appendChild(script);
     } else if (request.action == "closeiframe") {
         var newDiv = document.getElementById(divId);
